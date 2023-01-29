453483e898107f2cc4158437460acf8b07d4276e
CW1224 <xixiwang1224@gmail.com>
2023-01-29 11:10:57 +0000

    Added in the models needed and connected the django to a html base

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+STATUS = ((0, 'Unapproved'), (1, 'Approved'))
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reservations')
+    created_date = models.DateTimeField(auto_now=True)
+    booking_date = models.DateField(auto_now=False)
+    booking_time =models.TimeField(auto_now=False)
+    number_guests = models.IntegerField()
+    booking_comments = models.TextField(max_length=500, blank=True)
+    high_chair = models.BooleanField(null=False, blank=False, default=False)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-booking_date']
+
+    def __str__(self):
+        return self.title
+
+class User_application(models.Model):
+    user = models.TextField(max_length=50)
+    password = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=9)
+    email = models.EmailField()
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import generic
+from .models import Booking
 
 # Create your views here.
+
+def open_home_page(request):
+    return render(request, 'restaurant/base.html')
